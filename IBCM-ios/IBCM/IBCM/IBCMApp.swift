98126b932c3dc13f2d55606d93261c9b43889215
fansciecode <your.email@example.com>
2025-03-20 13:38:25 +0000

    Added iOS project structure

---
+++ IBCM-ios/IBCM/IBCM/IBCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBCMApp.swift
+//  IBCM
+//
+//  Created by kiran Naik on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct IBCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
